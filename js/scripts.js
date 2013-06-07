a4b0a96b054321ae03e896c44e1210e8f5f56a1d
zkellerdevelops <zkellerdevelops@gmail.com>
2013-06-07 13:27:45 -0500

    moved js scripts into dedicated scripts js file

---
+++ js/scripts.js
@@ -0,0 +1,48 @@
+$(document).ready(function() {
+
+ //Start of hover opacity effect
+  $('.social a, .prev, .next').hover(function() {
+    $(this).find('img').stop().fadeTo( 'fast', .2 );
+
+    }, function() {
+      $(this).find('img').stop().fadeTo( 'slow', 1 );
+    }); //End of hover
+
+ //start Fancybox
+  $(".fancybox").fancybox();
+ //end Fancybox
+
+ //SlideJS script
+  $(function(){
+    $('#slides').slides({
+      preload: true,
+      preloadImage: 'images/slide/loading.gif',
+      play: 10000,
+      pause: 2500,
+      hoverPause: true
+    });
+  });
+
+ //nav button page animation
+  $("#contactbutton").click(function(){
+    $('html, body').animate({
+      scrollTop: $("#contact").offset().top
+    }, 1500);
+    return false;
+  });
+
+  $("#portfoliobutton").click(function(){
+    $('html, body').animate({
+      scrollTop: $("#portfolio").offset().top
+    }, 1500);
+    return false;
+  });
+
+  $("#homebutton").click(function(){
+    $('html, body').animate({
+      scrollTop: $("#home").offset().top
+    }, 1500);
+    return false;
+  });
+
+});
